b917d1453968f3387119c2f1868af11790abf554
detrox20 <detrox2020@gmail.com>
2020-09-02 14:58:08 +0900

    feat: add detail redux

--- src/Redux/middlewares/saga.js
+++ src/Redux/middlewares/saga.js
@@ -1,10 +1,8 @@
-import {all} from 'redux-saga/effects'
-import { theaterSaga } from '../modules/resTheater';
-import { selectDataSaga } from '../modules/select';
+import { all } from "redux-saga/effects";
+import { theaterSaga } from "../modules/resTheater";
+import { selectDataSaga } from "../modules/select";
+import { movieDataSaga } from "../modules/detail";
 
-export default function* rootSaga(){
-    yield all([
-        theaterSaga(),
-        selectDataSaga()
-    ]);
+export default function* rootSaga() {
+  yield all([theaterSaga(), selectDataSaga(), movieDataSaga()]);
 }
--- src/Redux/modules/reducer.js
+++ src/Redux/modules/reducer.js
@@ -1,14 +1,13 @@
-import {connectRouter} from 'connected-react-router';
-import {combineReducers} from 'redux';
-import selectData from './select'
-import theaters from './resTheater'
-const reducer = history => (
-    combineReducers(
-        {
-            selectData
-            ,theaters
-            ,router : connectRouter(history)
-        }
-    )
-)
+import { connectRouter } from "connected-react-router";
+import { combineReducers } from "redux";
+import selectData from "./select";
+import theaters from "./resTheater";
+import detail from "./detail";
+const reducer = (history) =>
+  combineReducers({
+    selectData,
+    theaters,
+    detail,
+    router: connectRouter(history),
+  });
 export default reducer;
