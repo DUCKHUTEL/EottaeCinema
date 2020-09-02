3ca8459e8f0492ca998237f0a70acf75234d269b
detrox20 <detrox2020@gmail.com>
2020-09-02 14:55:52 +0900

    feat: create detail module

---
+++ src/Redux/modules/detail.js
@@ -0,0 +1,83 @@
+import moiveService from "../../Services/movieService";
+import { put, call, takeLatest } from "redux-saga/effects";
+
+const initialState = {
+  loading: false,
+  movieData: [],
+  error: null,
+};
+
+const prefix = "EOTTAECINEMA/detail";
+
+const GET_START = `${prefix}/GET_START`;
+const GET_SUCCESS = `${prefix}/GET_SUCCESS`;
+const GET_FAIL = `${prefix}/GET_FAIL`;
+
+const startGetMovieData = () => {
+  return {
+    type: GET_START,
+  };
+};
+
+const successGetMovieData = (movieData) => {
+  return {
+    type: GET_SUCCESS,
+    movieData,
+  };
+};
+
+const failGetMovieData = (err) => {
+  return {
+    type: GET_FAIL,
+    err,
+  };
+};
+
+function* getMovieSaga() {
+  yield put(startGetMovieData);
+  try {
+    const movieData = yield call(moiveService.getMovieData());
+    yield put(successGetMovieData(movieData));
+    console.log("movieData", movieData);
+  } catch (err) {
+    yield put(failGetMovieData(err));
+  }
+}
+const GET_MOVIE_SAGA = "GET_MOVIE_SAGA";
+export const getMovieDataSagaActionCreator = () => {
+  return {
+    type: GET_MOVIE_SAGA,
+  };
+};
+
+export function* movieDataSaga() {
+  yield takeLatest(GET_MOVIE_SAGA, getMovieSaga);
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_START:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case GET_SUCCESS:
+      return {
+        loading: false,
+        movieData: action.movieData,
+        error: null,
+      };
+
+    case GET_FAIL:
+      return {
+        ...state,
+        loading: true,
+        error: action.err,
+      };
+
+    default:
+      return state;
+  }
+}
