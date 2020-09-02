efb77400356a5e9944a9e91d8531a63e964a65ec
detrox20 <detrox2020@gmail.com>
2020-09-02 14:57:39 +0900

    docs: saga test

--- src/Containers/DetailPageContainer.jsx
+++ src/Containers/DetailPageContainer.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import DetailVisualTop from "../Components/Detail1/DetailVisualTop";
 import DetailContent from "../Components/Detail2/DetailContent";
+import { getMovieDataSagaActionCreator } from "../Redux/modules/detail";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function DetailPageContainer() {
+  const movieData = useSelector((state) => state.detail);
+
+  const dispatch = useDispatch();
+  const getMovieData = React.useCallback(() => {
+    dispatch(getMovieDataSagaActionCreator());
+  }, [dispatch]);
+
+  console.log("movieData", movieData);
   return (
     <>
-      <DetailVisualTop />
+      <DetailVisualTop getMovieData={getMovieData} />
       <DetailContent />
     </>
   );
