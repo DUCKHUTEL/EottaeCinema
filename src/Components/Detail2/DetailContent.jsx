8f91ce6d6df8530a163ff94fc1c6c0fde2647444
detrox20 <detrox2020@gmail.com>
2020-09-03 17:48:11 +0900

    docs: separate component
    separate DetailContent component and create DetailInfo component

--- src/Components/Detail2/DetailContent.jsx
+++ src/Components/Detail2/DetailContent.jsx
@@ -85,53 +85,6 @@ export default function DetailContent() {
           </div>
         </div>
       </div>
-      <div>
-        <div></div>
-        <div className={styles["movie-info2"]}>
-          <button>
-            <span>평점 및 관람평</span>
-          </button>
-          <div>
-            <em>총 평점</em>
-            <strong>8</strong>
-            <span>10</span>
-          </div>
-          <div className="review-box">
-            <div className="star-score-box">
-              <strong>
-                <em>8</em>점
-              </strong>
-              <div>
-                <div>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                  <span>별</span>
-                </div>
-              </div>
-            </div>
-            <div>
-              <textarea placeholder="평점 및 영화 관람평을 작성해주세요. 한글 110자 / 영문 220자 작성 가능합니다."></textarea>
-              <span>
-                <strong>0</strong>/<em>220</em>
-              </span>
-            </div>
-            <button>관람평 작성</button>
-          </div>
-        </div>
-        <div className="movie-info3">
-          <h5>포스터 스틸컷</h5>
-          <div>슬라이드</div>
-          <div>뷰</div>
-          <div>감독 및 배우</div>
-        </div>
-      </div>
     </div>
   );
 }
