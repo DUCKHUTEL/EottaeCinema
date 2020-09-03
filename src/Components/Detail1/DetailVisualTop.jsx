6725b761ea6f7e6e6f02f03154b7d62e243dd1a4
detrox20 <detrox2020@gmail.com>
2020-09-03 17:40:21 +0900

    feat: create top visual slider of detail page
    1. using react-owl-carousel library
    2. insert poster through API url

--- src/Components/Detail1/DetailVisualTop.jsx
+++ src/Components/Detail1/DetailVisualTop.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import styles from "./DetailVisualTop.module.scss";
+import OwlCarousel from "react-owl-carousel";
+import "./DetailVisualTop.scss";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
-export default function DetailVisualTop(props) {
-  console.log(props.getMovieData, props.movieData);
+export default function DetailVisualTop({ movieData, getMovieData }) {
+  React.useEffect(() => {
+    getMovieData();
+  }, [getMovieData]);
+  const stllsURLArray = movieData.stlls;
 
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <div>
-              {/* <div className={styles["stage-box"]}></div>
-              <div className={styles["stage-box"]}></div>
-              <div className={styles["stage-box"]}></div>
-              <div className={styles["stage-box"]}></div>
-              <div className={styles["stage-box"]}></div>
-              <div className={styles["stage-box"]}></div>
-              <div className={styles["stage-box"]}></div> */}
-            </div>
-          </div>
-        </div>
-        <div></div>
-        <div></div>
-      </div>
+    <div className={styles["top-visual-wrap"]}>
+      <OwlCarousel
+        className="owl-theme top-visual-box"
+        items={1}
+        loop
+        margin={10}
+        nav
+        autoplay
+        autoplayTimeout={3000}
+        autoplaySpeed={500}
+      >
+        {stllsURLArray === undefined ||
+          stllsURLArray.map((url) => {
+            return (
+              <div className="item">
+                <img src={`${url}`} alt="poster" />
+              </div>
+            );
+          })}
+      </OwlCarousel>
     </div>
   );
 }
