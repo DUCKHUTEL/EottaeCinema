6122e00872d4b15de4e44d7aad4af7b6ff880cab
detrox20 <detrox2020@gmail.com>
2020-09-04 22:08:25 +0900

    feat: combine API, DB Data

--- src/Containers/DetailPageContainer.jsx
+++ src/Containers/DetailPageContainer.jsx
@@ -3,27 +3,46 @@ import DetailVisualTop from "../Components/Detail1/DetailVisualTop";
 import DetailContent from "../Components/Detail2/DetailContent";
 import DetailInfo from "../Components/Detail3/DetailInfo";
 import { getMovieDataSagaActionCreator } from "../Redux/modules/detail";
+import { startGetMoviesActionCreator } from "../Redux/modules/movies";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 
 export default function DetailPageContainer() {
-  const movieData = useSelector((state) => state.detail.movieData);
+  const movieAPIData = useSelector((state) => state.detail.movieData);
+  const moviesDBData = useSelector((state) => state.movies.movies);
+  console.log("movieDBData", moviesDBData);
 
   const dispatch = useDispatch();
-  const getMovieData = React.useCallback(() => {
-    dispatch(getMovieDataSagaActionCreator());
+
+  const getMovieAPIData = React.useCallback(() => {
+    dispatch(getMovieDataSagaActionCreator("시크릿 가든"));
   }, [dispatch]);
 
+  const getMoviesDBData = React.useCallback(() => {
+    dispatch(startGetMoviesActionCreator());
+  });
+
   useEffect(() => {
-    getMovieData();
+    getMovieAPIData();
   }, []);
 
-  console.log("movieData", movieData);
+  useEffect(() => {
+    getMoviesDBData();
+  }, []);
+
+  console.log("movieData", movieAPIData);
   return (
     <>
-      <DetailVisualTop movieData={movieData} getMovieData={getMovieData} />
-      <DetailContent />
-      <DetailInfo />
+      <DetailVisualTop
+        movieAPIData={movieAPIData}
+        getMovieAPIData={getMovieAPIData}
+      />
+      <DetailContent
+        movieAPIData={movieAPIData}
+        moviesDBData={moviesDBData}
+        getMoviesDBData={getMoviesDBData}
+      />
+      <DetailInfo movieAPIData={movieAPIData} moviesDBData={moviesDBData} />
     </>
   );
 }
