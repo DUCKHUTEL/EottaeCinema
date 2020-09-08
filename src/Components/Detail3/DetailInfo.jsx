59ae0db8661dcae686ea2f9de04f0bff240cdf83
detrox20 <detrox2020@gmail.com>
2020-09-08 19:00:21 +0900

    feat: separate components
    to make slider, separate DetailTrailer.jsx and DetailTrailer.jsx

--- src/Components/Detail3/DetailInfo.jsx
+++ src/Components/Detail3/DetailInfo.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import styles from "./DetailInfo.module.scss";
-import Scrollbars from "react-custom-scrollbars";
+import React from 'react';
+import styles from './DetailInfo.module.scss';
+import Scrollbars from 'react-custom-scrollbars';
+import DetailTrailer from './DetailTrailer';
+import DetailStillCut from './DetailStillCut';
 
 export default function DetailInfo({ movieAPIData, moviesDBData }) {
   const APIData = movieAPIData;
@@ -9,36 +11,44 @@ export default function DetailInfo({ movieAPIData, moviesDBData }) {
   console.log(APIData, DBData);
   return (
     <div className={styles.info}>
-      <div className={styles["movie-info1"]}>
-        <button>
+      <div className={styles['movie-info1']}>
+        <button className={styles['movie-info1-button']}>
           <span>영화정보</span>
         </button>
-        <div className={styles["tab-icon"]}>
-          <div className={styles["movie-tab-info1"]}>
-            <div className={styles["left-con"]}>
-              <strong className={styles["synopsys-text"]}>시놉시스</strong>
-              <div className={styles["synopsys-wrap"]}>
-                <div className={styles["synopsys-box"]}>
+        <div className={styles['tab-icon']}>
+          <div className={styles['movie-tab-info1']}>
+            <div className={styles['left-con']}>
+              <strong className={styles['synopsys-text']}>시놉시스</strong>
+              <div className={styles['synopsys-wrap']}>
+                <div className={styles['synopsys-box']}>
                   <Scrollbars
-                    style={{ width: "100%", height: "115px" }}
+                    style={{ width: '100%', height: '115px' }}
                     autoHide
                     autoHideTimeout={2000}
                     autoHideDuration={200}
                   >
-                    <div className={styles["synopsys-inner"]}>
+                    <div className={styles['synopsys-inner']}>
                       {APIData === undefined || APIData.synopsys}
                     </div>
                   </Scrollbars>
                 </div>
               </div>
             </div>
-            <div className={styles["right-con"]}>
+            <div className={styles['right-con']}>
               <strong>관람 선호도</strong>
             </div>
           </div>
+          <DetailTrailer
+            movieAPIData={movieAPIData}
+            moviesDBData={moviesDBData}
+          />
+          <DetailStillCut
+            movieAPIData={movieAPIData}
+            moviesDBData={moviesDBData}
+          />
         </div>
       </div>
-      <div className={styles["movie-info2"]}>
+      <div className={`${styles['movie-info2']} ${styles.none}`}>
         <button>
           <span>평점 및 관람평</span>
         </button>
@@ -77,10 +87,16 @@ export default function DetailInfo({ movieAPIData, moviesDBData }) {
         </div>
       </div>
       <div className="movie-info3">
-        <h5>포스터 스틸컷</h5>
-        <div>슬라이드</div>
-        <div>뷰</div>
         <div>감독 및 배우</div>
+        <iframe
+          title="예고편"
+          width="920"
+          height="517.5"
+          src="https://www.youtube.com/embed/919GruRSZzE"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
       </div>
     </div>
   );
