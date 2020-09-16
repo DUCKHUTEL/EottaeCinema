aedd8ae3f1fb4c83081ac2f7372da25910c64e2f
soda-cloud <linn45577@gmail.com>
2020-09-17 01:21:07 +0900

    Create Fromik

--- src/Components/SignInPortal/SigInPortal.jsx
+++ src/Components/SignInPortal/SigInPortal.jsx
@@ -1,29 +1,48 @@
 import React, { useCallback, useRef } from 'react';
 import styles from './SignInPortal.module.scss';
 import UserService from '../../Services/userService';
+import { Formik, Form, Field } from 'formik';
 
-function SignInPortal({ login, nickName, token }) {
-  const emailRef = useRef();
+function SignInPortal({ login, checkId, nickName, token }) {
+  const idRef = useRef();
   const passwordRef = useRef();
 
-  const click = useCallback(() => {
-    const id = emailRef.current.value;
+  const clickLogin = useCallback(() => {
+    const id = idRef.current.value;
     const password = passwordRef.current.value;
 
     if (id === '' || password === '') return;
+
     // console.log(login);
     login(id, password);
   }, []);
 
+  const clickCheckId = useCallback(() => {
+    const id = idRef.current.value;
+
+    if (id === '') return;
+
+    checkId(id);
+  }, []);
+
   return (
     <main>
+      <Formik></Formik>
       <section>
         <div>
           <h1>로그인</h1>
-          <input type="text" ref={emailRef}></input>
-          <button></button>
+          <input type="text" ref={idRef}></input>
+          <p></p>
+          <button
+            className={styles['btn-checkId']}
+            onClick={clickCheckId}
+          ></button>
           <input type="password" ref={passwordRef}></input>
-          <button className={styles['btn-signin']} onClick={click}></button>
+          <button
+            className={styles['btn-signIn']}
+            onClick={clickLogin}
+          ></button>
+          <button className={styles['btn-signUp']}></button>
         </div>
       </section>
     </main>
