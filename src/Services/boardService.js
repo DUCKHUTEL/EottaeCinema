e9b1c30612d11ea0b4822b3db0606b5d5c9e7c4a
detrox20 <detrox2020@gmail.com>
2020-09-17 13:34:38 +0900

    feat: complete boardService.js
    creating a service to use the review

--- src/Services/boardService.js
+++ src/Services/boardService.js
@@ -2,22 +2,76 @@ import axios from 'axios';
 
 const REVIEW_URL = 'https://eottae-cinema.herokuapp.com/';
 
-const ON_TIME = 'boardOnTime';
-const ON_FAVOR = 'boardOnFavor';
+const GET_ON_TIME = 'boardOnTime';
+const GET_ON_FAVOR = 'boardOnFavor';
+const ADD_REVIEW = 'appBoard';
+const PATCH_REVIEW = 'patchBoard';
+const DELETE_REVIEW = 'deleteBoard';
+const LIKE_REVIEW = 'patchStar';
 
 export default class boardService {
-  static async getReviewOnTimeData() {
-    const res = await axios.get(`${REVIEW_URL}${ON_TIME}?movie=테넷&count=1`);
+  static async getReviewsOnTime(movie, count) {
+    const res = await axios.get(
+      `${REVIEW_URL}${GET_ON_TIME}?movie=${movie}&count=${count}`,
+    );
 
-    console.log('reviewOnTime', res.data);
+    console.log(
+      'reviewOnTime',
+      `${REVIEW_URL}${GET_ON_TIME}?movie=${movie}&count=${count}`,
+      res.data,
+      movie,
+      count,
+    );
 
     return res.data;
   }
 
-  static async getReviewOnFavorData() {
-    const res = await axios.get(`${REVIEW_URL}${ON_FAVOR}?movie=테넷&count=1`);
+  static async getReviewsOnFavor(movie, count) {
+    const res = await axios.get(
+      `${REVIEW_URL}${GET_ON_FAVOR}?movie=${movie}&count=${count}`,
+    );
 
     console.log('reviewOnFavor', res.data);
+
+    return res.data;
+  }
+
+  static async addReview(token, movie, starPoint, content, nickName) {
+    const res = await axios.post(
+      `${REVIEW_URL}${ADD_REVIEW}`,
+      { movie, starPoint, content, nickName },
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+
+    return res.data;
+  }
+
+  static async editReview(token, id, starPoint, content) {
+    const res = await axios.patch(
+      `${REVIEW_URL}${PATCH_REVIEW}`,
+      { id, starPoint, content },
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+
+    return res.data;
+  }
+
+  static async deleteReview(token, id) {
+    const res = await axios.delete(
+      `${REVIEW_URL}${DELETE_REVIEW}`,
+      { id },
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+
+    return res.data;
+  }
+
+  static async clickLikeReview(token, id, status) {
+    const res = await axios.patch(
+      `${REVIEW_URL}${LIKE_REVIEW}`,
+      { id, status },
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
 
     return res.data;
   }
