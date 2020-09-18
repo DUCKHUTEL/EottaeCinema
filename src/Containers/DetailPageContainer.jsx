769d7f76b736c792ddd480c547605fe7a84d296e
detrox20 <detrox2020@gmail.com>
2020-09-19 01:56:45 +0900

    docs: remove unnecessary code that isn't in use
    ex) console.log, not using

--- src/Containers/DetailPageContainer.jsx
+++ src/Containers/DetailPageContainer.jsx
@@ -6,22 +6,19 @@ import { getMovieDataSagaActionCreator } from '../Redux/modules/detail';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { startGetMoviesActionCreator } from '../Redux/modules/movies';
+import { Redirect } from 'react-router-dom';
 
 export default function DetailPageContainer() {
   const selectedMovie = useSelector((state) => state.router.location.state);
   const movieAPIData = useSelector((state) => state.detail.movieData);
   const moviesDBData = useSelector((state) => state.movies.movies);
-  const loading = useSelector((state) => state.detail.loading);
-
-  console.log('selectedMovie', selectedMovie.selectTitle);
-  console.log('loading', loading);
-  console.log('movieDBData', moviesDBData);
+  // const loading = useSelector((state) => state.detail.loading);
 
   const dispatch = useDispatch();
 
   const getMovieAPIData = React.useCallback(() => {
-    dispatch(getMovieDataSagaActionCreator(selectedMovie.selectTitle));
-  }, []);
+    dispatch(getMovieDataSagaActionCreator(selectedMovie));
+  }, [dispatch, selectedMovie]);
 
   const getMoviesDBData = React.useCallback(() => {
     dispatch(startGetMoviesActionCreator());
@@ -29,31 +26,30 @@ export default function DetailPageContainer() {
 
   useEffect(() => {
     getMovieAPIData();
-  }, []);
-
-  useEffect(() => {
     getMoviesDBData();
-  }, []);
+  }, [getMovieAPIData, getMoviesDBData]);
+
+  if (selectedMovie === undefined) {
+    return <Redirect to="/" />;
+  }
 
   const DBData = moviesDBData.find(
-    (DBdata) => DBdata.movieTitle === selectedMovie.selectTitle,
+    (DBdata) => DBdata.movieTitle === selectedMovie,
   );
 
   return (
     <>
-      <DetailVisualTop
-        movieAPIData={movieAPIData}
-        getMovieAPIData={getMovieAPIData}
-      />
+      <DetailVisualTop DBData={DBData} />
       <DetailContent
-        movieAPIData={movieAPIData}
+        APIData={movieAPIData}
         DBData={DBData}
         getMoviesDBData={getMoviesDBData}
       />
       <DetailInfo
-        movieAPIData={movieAPIData}
+        APIData={movieAPIData}
         DBData={DBData}
         getMoviesDBData={getMoviesDBData}
+        selectedMovie={selectedMovie}
       />
     </>
   );
