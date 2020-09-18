5820518b445cabde88b06865cc15b082b607bb74
detrox20 <detrox2020@gmail.com>
2020-09-19 01:59:21 +0900

    feat: add right dependency
    right dependency

--- src/Containers/DetailReviewContainer.jsx
+++ src/Containers/DetailReviewContainer.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import DetailReview from '../Components/DetailReviewComponent/DetailReview';
 import { getReviewsSagaActionCreator } from '../Redux/modules/board';
 
 export default function DetailReviewContainer({
   infoState,
   reviewInfoClick,
-  movieTitle,
+  DBData,
+  selectedMovie,
 }) {
   const reviewsData = useSelector((state) => state.board.reviews);
-  console.log('movieTitle', movieTitle);
 
   const dispatch = useDispatch();
 
   const getReviews = React.useCallback(() => {
-    dispatch(getReviewsSagaActionCreator(movieTitle, 1));
-  }, [dispatch, movieTitle]);
+    dispatch(getReviewsSagaActionCreator(selectedMovie, 1));
+  }, [dispatch, selectedMovie]);
 
   useEffect(() => {
     getReviews();
-  }, []);
-
-  console.log('reviews', movieTitle, reviewsData);
+  }, [getReviews]);
 
   return (
-    <DetailReview infoState={infoState} reviewInfoClick={reviewInfoClick} />
+    <DetailReview
+      infoState={infoState}
+      reviewInfoClick={reviewInfoClick}
+      DBData={DBData}
+      selectedMovie={selectedMovie}
+      reviewsData={reviewsData}
+    />
   );
 }
