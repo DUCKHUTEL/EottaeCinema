ca058f9044ccfabb9c8d5968aaaa443cd3ed656a
detrox20 <detrox2020@gmail.com>
2020-09-19 02:10:21 +0900

    issue: unable to get director & actor images
    to be revise in the future

--- src/Components/DetailPeopleComponent/DetailPeople.jsx
+++ src/Components/DetailPeopleComponent/DetailPeople.jsx
@@ -1,21 +1,56 @@
 import React from 'react';
 import styles from './DetailPeople.module.scss';
 
-export default function DetailPeople({ movieAPIData, moviesDBData }) {
+export default function DetailPeople({ APIData, DBData }) {
   return (
     <div className={styles['movie-people-box']}>
       <div className={styles['movie-people-text']}>감독 및 배우</div>
       <div className={styles['movie-people-list-box']}>
         <ul>
           <li>
-            <div className={styles['picture']}></div>
+            <div className={styles['picture']}>
+              <img
+                src={`https://caching.lottecinema.co.kr//Media/MovieFile/${
+                  DBData === undefined ||
+                  DBData.movieDirectorImg.substring(
+                    1,
+                    DBData.movieDirectorImg.length - 1,
+                  )
+                }`}
+                alt="감독"
+              />
+            </div>
             <div className={styles['people-info-box']}>
               <div className={styles['people-inner']}>
-                <span className={styles['people-name']}>크리스토퍼 놀란</span>
+                <span className={styles['people-name']}>{`${
+                  DBData === undefined || DBData.movieDirector
+                }`}</span>
                 <span className={styles['people-cast-name']}>감독</span>
               </div>
             </div>
           </li>
+          {DBData === undefined ||
+            DBData.movieActors.split(';').map((url, index) => {
+              return (
+                <li key={index}>
+                  <div className={styles['picture']}>
+                    <img
+                      src={`https://caching.lottecinema.co.kr//Media/MovieFile/${url.substring(
+                        1,
+                        DBData.movieDirectorImg.length - 1,
+                      )}`}
+                      alt="배우"
+                    />
+                  </div>
+                  <div className={styles['people-info-box']}>
+                    <div className={styles['people-inner']}>
+                      {/* <span className={styles['people-name']}>{APIData === undefined || APIData.actors[index]}</span>
+                    <span className={styles['people-cast-name']}>{DBData === undefined || DBData.movieCastName.split(';')[index].substring(1, DBData.movieCastName.split(';')[index].length - 1)}</span> */}
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
