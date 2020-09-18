d3071c3316c7510596f4988f477c37ad56e7a67f
detrox20 <detrox2020@gmail.com>
2020-09-19 02:17:54 +0900

    feat: remove API data & use DB data
    must use HashRouter

--- src/Components/DetailStillCutComponent/DetailStillCut.jsx
+++ src/Components/DetailStillCutComponent/DetailStillCut.jsx
@@ -3,14 +3,17 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import './DetailStillCut.scss';
+import { Link, Route } from 'react-router-dom';
+import { history } from '../../Redux/create';
 
-export default function DetailStillCut({ movieAPIData, DBData }) {
-  const APIData = movieAPIData;
-
-  console.log(APIData, DBData);
+export default function DetailStillCut({ DBData }) {
+  console.log(history);
   return (
     <div className="movie-tab-info3">
-      <div className="still-cut-text">포스터 · 스틸컷 ()</div>
+      <div className="still-cut-text">
+        포스터 · 스틸컷 (
+        {DBData === undefined || DBData.movieStillCut.split(';').length})
+      </div>
       <div className="poster-wrap">
         <OwlCarousel
           className="owl-theme"
@@ -20,28 +23,60 @@ export default function DetailStillCut({ movieAPIData, DBData }) {
           dots={false}
           dotData={true}
           URLhashListener={true}
+          startPosition={'URLHash'}
         >
-          {APIData.stlls === undefined ||
-            APIData.stlls.map((url, index) => {
+          {DBData === undefined ||
+            DBData.movieStillCut.split(';').map((url, i) => {
               return (
-                <div className="item" key={index}>
-                  <a href="#">
-                    <img src={`${url}`} alt="poster" />
-                  </a>
+                <div
+                  className="item"
+                  key={i}
+                  id={`hashStillCut_${i}`}
+                  data-hash={`#hashStillCut_${i}`}
+                >
+                  <Link to={`#hashStillCut-${i + 1}`}>
+                    <img
+                      src={`https://caching.lottecinema.co.kr//Media/MovieFile/${url.substring(
+                        1,
+                        url.length - 1,
+                      )}`}
+                      alt={`트레일러${i}`}
+                    />
+                  </Link>
                 </div>
               );
             })}
         </OwlCarousel>
       </div>
       <div className="still-cut-wrap">
-        <OwlCarousel className="owl-theme" items={1} loop margin={20} nav dots>
-          {APIData.stlls === undefined ||
-            APIData.stlls.map((url, index) => {
+        <OwlCarousel
+          className="owl-theme"
+          items={1}
+          l
+          oop
+          margin={20}
+          nav
+          dots
+          URLhashListener={true}
+          startPosition={'URLHash'}
+        >
+          {DBData === undefined ||
+            DBData.movieStillCut.split(';').map((url, i) => {
               return (
-                <div className="item" key={index}>
-                  {/* <a href="#"> */}
-                  <img src={`${url}`} alt="poster" />
-                  {/* </a> */}
+                <div
+                  className="item"
+                  key={i}
+                  data-hash={`#hashStillCut_${i + 1}`}
+                >
+                  {/* <Route path={`/hashStillCut_${i}`}> */}
+                  <img
+                    src={`https://caching.lottecinema.co.kr//Media/MovieFile/${url.substring(
+                      1,
+                      url.length - 1,
+                    )}`}
+                    alt={`트레일러${i}`}
+                  />
+                  {/* </Route> */}
                 </div>
               );
             })}
