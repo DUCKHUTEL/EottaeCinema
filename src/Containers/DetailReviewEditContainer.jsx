6449bbf7d7092aae5331808af87ec3c31ea6fdee
detrox20 <detrox2020@gmail.com>
2020-09-21 07:51:49 +0900

    feat: complete edit review container
    1. to edit review, create container
    2. complete

---
+++ src/Containers/DetailReviewEditContainer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import DetailReviewEdit from '../Components/DetailReviewEditComponent/DetailReviewEdit';
+import { patchReviewSagaActionCreator } from '../Redux/modules/board';
+
+export default function DetailReviewEditContainer({
+  movie,
+  count,
+  id,
+  starPoint,
+  content,
+  setState,
+}) {
+  const dispatch = useDispatch();
+
+  const editReview = React.useCallback(
+    (movie, count, id, starPoint, content) => {
+      dispatch(
+        patchReviewSagaActionCreator(movie, count, id, starPoint, content),
+      );
+    },
+    [dispatch],
+  );
+
+  return (
+    <DetailReviewEdit
+      movie={movie}
+      id={id}
+      starPoint={starPoint}
+      content={content}
+      setState={setState}
+      editReview={editReview}
+    />
+  );
+}
