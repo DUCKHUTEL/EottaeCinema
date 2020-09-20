6ce9a1e24a8d27ea2e9ea63981b1597d3b7e4469
detrox20 <detrox2020@gmail.com>
2020-09-21 07:58:06 +0900

    feat: create delete button to delete review
    complete

---
+++ src/Components/DetailReviewsListComponent/Buttons/ReviewDeleteButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from '../DetailReviewsList.module.scss';
+import { deleteReviewSagaActionCreator } from '../../../Redux/modules/board';
+import { useDispatch } from 'react-redux';
+
+export default function ReviewDeleteButton({ movie, count, id }) {
+  const dispatch = useDispatch();
+
+  const deleteReview = React.useCallback(
+    (movie, count, id) => {
+      dispatch(deleteReviewSagaActionCreator(movie, count, id));
+    },
+    [dispatch],
+  );
+
+  const click = () => {
+    deleteReview(movie, id);
+  };
+
+  return (
+    <button className={styles.edit} onClick={click}>
+      삭제
+    </button>
+  );
+}
